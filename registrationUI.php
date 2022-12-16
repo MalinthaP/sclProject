c27bf0d736984b92e9e7572fca71099cb9b25449
Malintha P Senadheera <main.malinthapsenadheera@gmail.com>
2022-12-16 16:25:04 +0530

    First commit|user regitraton

---
+++ registrationUI.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="registrationStyle.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="form">
+        <h1 class="note"> Wellcome! </h1>
+        <div class="form-item">
+            <form action="" method="POST" >
+                <input type="text" name="user" id="name" placeholder="Enter Username">
+                <br>
+                <input type="password" name="password" id="password" placeholder="Enter Password">
+                <br>
+                <input type="submit" value="Sing Up" id="btn" name="singup">
+            </form>  
+        </div>
+    </div>
+</body>
+</html>
