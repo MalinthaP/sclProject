ae88f9e05a3e75a61c437e989f8afb2ef1fee192
Malintha P Senadheera <main.malinthapsenadheera@gmail.com>
2022-12-16 16:27:12 +0530

    First commit|connet with DB

---
+++ db_conn.php
@@ -0,0 +1,12 @@
+<!-- connection with detabace -->
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "ashirwadaDB";
+    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
+    if($conn->connect_error){
+        die("Connection failed" .$conn->connect_error);
+    }
+    // echo "Connection successful"
+?>
