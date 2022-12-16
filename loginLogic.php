bd9ac32cf9bacba96df00389c893a5a36c40a12c
Malintha P Senadheera <main.malinthapsenadheera@gmail.com>
2022-12-16 16:28:32 +0530

    First commit|user login logic

---
+++ loginLogic.php
@@ -0,0 +1,23 @@
+<?php
+    include('db_conn.php');
+    if (isset($_POST['login'])) {
+        $username = $_POST['user'];
+        $password = $_POST['password'];
+
+        $sql = "select * from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+        
+        if($count == 1){  
+            header("location:homePageUI.php");
+        }  
+        else{  
+            echo 
+            '<script>
+                window.location.href = "index.php";
+                alert("Login failed. Invalid username or password!!")
+            </script>';
+        }     
+    }
+    ?>
