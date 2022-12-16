adb79fb03438b596808e6d714a2792ab75d54c23
Malintha P Senadheera <main.malinthapsenadheera@gmail.com>
2022-12-16 16:22:12 +0530

    First commit|first page

---
+++ pageGateUI.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pageGateStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="gateBox">
+        <!-- <h1 class="note01">Wellcome Our Site</h1> -->
+        <h2 class="note02">Pleas Select Your Choice</h2>
+        <div class="boxItem">
+            <a href="loginPageUI.php" class="login"><button>Login</button></a>
+            <br>
+            <a href="registrationUI.php" class="singUp"><button>Sing In</button></a>
+        </div>
+    </div>
+</body>
+</html>
